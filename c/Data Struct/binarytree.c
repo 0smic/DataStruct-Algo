d5427569947e3db680697cd7234fed7b99deccbd
Gokul <146622216+0smic@users.noreply.github.com>
2025-04-30 00:57:10 +0530

    Create binarytree.c

---
+++ c/Data Struct/binarytree.c
@@ -0,0 +1,71 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<stdbool.h>
+
+typedef struct treenode {
+   int value;
+   struct treenode *left;
+   struct treenode *right;
+} treenode_t;
+
+treenode_t* createnode(int);
+void printtree(treenode_t*);
+void main(){
+    treenode_t *n1 = createnode(10);
+    treenode_t *n2 = createnode(11);
+    treenode_t *n3 = createnode(12);
+    n1->left = n2;
+    n2->right = n3;
+    printtree(n1);
+}
+
+treenode_t* createnode(int value){
+    treenode_t *node = malloc(sizeof(treenode_t));
+    if (node == NULL){
+        printf("allocation error!!");
+        return NULL;
+    }
+    node->value = value;
+    node->left = NULL;
+    node->right = NULL;
+    return node;
+}
+
+bool insertNode(int value,treenode_t **root){
+    if(*root == NULL){
+        *root = createnode(value);
+        return true;
+    }
+    if (root->value == value){
+        return false;
+    }
+    else if (root->value < value){
+        if(root->right == NULL){
+            root->right = createnode(value);
+            return true;
+        }
+        insertNode(value, root->right);
+    }
+
+    else if (root->value > value){
+        if(root->left == NULL){
+            root->left = createnode(value);
+            return true;
+        }
+        insertNode(value, root->left);
+    }  
+}
+
+void printtree(treenode_t *root){
+    if (root == NULL){
+        printf("---NULL---\n");
+        return;
+    }
+    printf("value %d\n",root->value);
+    printf("LEFT: \n");
+    printtree(root->left);
+    printf("RIGHT: \n");
+    printtree(root->right);
+    
+    
+}
